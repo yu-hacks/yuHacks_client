17ee5fe4f29031202e0e9df55809d18f7305858b
Humza987 <humzainam@gmail.com>
2023-06-19 14:51:08 -0400

    YorkUHacksWatermark Component Added

---
+++ src/components/common/YorkUHacksWatermark.component.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react'
+
+const YorkUHacksWatermark :FC = () => {
+  return (
+    <>
+
+    <div className="flex justify-center items-center h-screen transform rotate-90">
+  <p className=" font-ndot47 lg:text-7xl text-2xl sm:text-4xl leading-9 text-gray-200"> 
+  yorku_hacks()
+  </p>
+    </div>
+
+    </>
+  )
+}
+
+export default YorkUHacksWatermark
