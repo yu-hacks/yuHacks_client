5da4abb131ee20fc70402aac9f8bd963cab9640f
tranguv <89886131+tranguv@users.noreply.github.com>
2023-06-30 12:07:25 -0400

    add usestate

--- src/components/common/HackerCard.component.tsx
+++ src/components/common/HackerCard.component.tsx
@@ -5,25 +5,40 @@ import Image from 'next/image';
 import ImageWithFallback from './ImageFallback';
 import '../../styles/HackerCard.css';
 
-interface Props {
-  name: string;
-}
+//api
+// import { getUserData } from '@/utils/http-request';
 
-const HackerCard: FC<Props> = (props) => {
+const HackerCard = () => {
+  const [image, setImage] = useState(avt);
+  const [name, setName] = useState('');
 
+  //will be removed later
+  setName('John Doe');
+  setImage(avt);
+
+  // useEffect(() => {
+  //   const userData = getUserData();
+  //   //hard code for now
+  //   setName('John Doe');
+  //   setImage(avt);
+
+  // setName(userData.name);
+  // setImage(userData.avatar);
+  // }
+  //   , []);
   return (
     <>
       <div className="max-w-sm rounded overflow-hidden shadow-lg card-style">
         <div className="px-6 py-4">
           <div className="rectangle"></div>
-          <ImageWithFallback src={avt} fallbackSrc={avt}
+          <ImageWithFallback src={image} fallbackSrc={avt}
             className="w-30 h-30 rounded-full mx-auto bg-white"
             alt="Picture of the author"
             width={94.047}
             height={94.047}
           />
           <p className="hcard-name font-ndot47">
-            {props.name}
+            {name}
           </p>
           <p className='font-ndot47 yuhack'>yu_hacks(2023)</p>
         </div>
