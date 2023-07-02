ca8d909545dba1b2bb0e0ffe1b61eae9d250f77a
Dang-Tran <trmdang@my.yorku.ca>
2023-07-02 19:26:56 -0400

    Update ChallengeCard.component.tsx

--- src/components/common/ChallengeCard.component.tsx
+++ src/components/common/ChallengeCard.component.tsx
@@ -59,6 +59,18 @@ type Props = {
 
 const Card: FC<Props> = ({ deg, index, text, handleClick }) => {
 
+    const CardVariants = {
+        begin: () => ({
+            opacity: 0,
+            rotate: 0,
+        }),
+        end: (index: number) => ({
+            opacity: 1,
+            zIndex: cardData.length - index,
+            rotate: deg,
+        }),
+    }
+
     return (
         <motion.div
             className={`absolute rounded-lg shadow-md w-52 h-64 sm:w-72 sm:h-[400px] bg-[#FEFFFC] flex items-center`}
@@ -66,10 +78,10 @@ const Card: FC<Props> = ({ deg, index, text, handleClick }) => {
                 cursor: index === 0 ? 'grab' : 'default',
             }}
             onDragEnd={handleClick}
-            animate={{
-                zIndex: cardData.length - index,
-                rotate: deg,
-            }}
+            custom={index}
+            variants={CardVariants}
+            initial="begin"
+            animate="end"
             drag='x'
             dragConstraints={{ left: 0, right: 0 }}
         >
