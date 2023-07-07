5c116a542001051e17addba14563b9853f2f39dd
chinyee8 <laichinyee1101@gmail.com>
2023-07-07 08:14:08 -0400

    Update AccentedButton.component.tsx

--- src/components/common/AccentedButton.component.tsx
+++ src/components/common/AccentedButton.component.tsx
@@ -4,16 +4,17 @@ import {MdDownload} from 'react-icons/md'
 import {FaChevronRight} from 'react-icons/fa'
 
 // example
-{/* <PrimaryButton name='sec. Direction Button' buttonType='action' primary={true} fullWidth={true}/> */}
-
+{/* <AccentedButton name='sec. Direction Button' buttonType='action' primary={true} fullWidth={true} onClick={}/> */}
+/
 type Props = {
   name: string;
   buttonType: string;
   primary: boolean;
   fullWidth: boolean;
+  onClick: () => void;
 };
 
-const AccentedButton: FC<Props> = ({name, buttonType, primary, fullWidth}) => {
+const AccentedButton: FC<Props> = ({name, buttonType, primary, fullWidth, onClick}) => {
   const normalPrimary = `flex px-12 py-2 transition-all text-white text-base bg-[var(--bg-main-accent)] border-2 border-solid border-black/[.13] shadow-md hover:border-4 focus:border-4 focus:shadow-lg rounded-xl font-HindMadurai font-semibold`;
   const normalSecondary =`flex px-12 py-2 transition-all text-black text-base hover:shadow-md hover:rounded-xl focus:shadow-lg focus:rounded-xl font-HindMadurai font-semibold`;
   const fullPrimary = 'w-full w-min-[300px] flex justify-center py-2 transition-all text-white text-base bg-[var(--bg-main-accent)] border-2 border-solid border-black/[.13] shadow-md hover:border-4 focus:border-4 focus:shadow-lg rounded-xl font-HindMadurai font-semibold';
@@ -24,19 +25,19 @@ const AccentedButton: FC<Props> = ({name, buttonType, primary, fullWidth}) => {
             if(buttonType === 'default'){
               return (
                 <div className={full}>
-                  <button className={fullPrimary}>{name}</button> 
+                  <button onClick={onClick} className={fullPrimary}>{name}</button> 
                 </div>
               )
              }else if(buttonType === 'direction'){
               return (
                 <div className={full}>
-                  <button className={fullPrimary}>{name}&ensp;<FaChevronRight className='h-6 w-6 py-1'/></button> 
+                  <button onClick={onClick} className={fullPrimary}>{name}&ensp;<FaChevronRight className='h-6 w-6 py-1'/></button> 
                 </div>
               );
              }else if(buttonType === 'action'){
               return (
                 <div className={full}>
-                  <button className={fullPrimary}><MdDownload className='h-7 w-7 py-[2px]'/>&ensp;{name}</button> 
+                  <button onClick={onClick} className={fullPrimary}><MdDownload className='h-7 w-7 py-[2px]'/>&ensp;{name}</button> 
                 </div>
               );
              }
@@ -44,19 +45,19 @@ const AccentedButton: FC<Props> = ({name, buttonType, primary, fullWidth}) => {
             if(buttonType === 'default'){
               return (
                 <div className={full}>
-                  <button className={fullSecondary}>{name}</button> 
+                  <button onClick={onClick} className={fullSecondary}>{name}</button> 
                 </div>
               )
              }else if(buttonType === 'direction'){
               return (
                 <div className={full}>
-                  <button className={fullSecondary}>{name}&ensp;<FaChevronRight className='h-6 w-6 py-1'/></button> 
+                  <button onClick={onClick} className={fullSecondary}>{name}&ensp;<FaChevronRight className='h-6 w-6 py-1'/></button> 
                 </div>
               );
              }else if(buttonType === 'action'){
               return (
                 <div className={full}>
-                  <button className={fullSecondary}><MdDownload className='h-7 w-7 py-[2px]'/>&ensp;{name}</button> 
+                  <button onClick={onClick} className={fullSecondary}><MdDownload className='h-7 w-7 py-[2px]'/>&ensp;{name}</button> 
                 </div>
               );
              }
@@ -66,29 +67,29 @@ const AccentedButton: FC<Props> = ({name, buttonType, primary, fullWidth}) => {
           if(primary){
             if(buttonType === 'default'){
               return (
-                <button className={normalPrimary}>{name}</button> 
+                <button onClick={onClick} className={normalPrimary}>{name}</button> 
               )
              }else if(buttonType === 'direction'){
               return (
-                <button className={normalPrimary}>{name}&ensp;<FaChevronRight className='h-6 w-6 py-1'/></button> 
+                <button onClick={onClick} className={normalPrimary}>{name}&ensp;<FaChevronRight className='h-6 w-6 py-1'/></button> 
               );
              }else if(buttonType === 'action'){
               return (
-                <button className={normalPrimary}><MdDownload className='h-7 w-7 py-[2px]'/>&ensp;{name}</button> 
+                <button onClick={onClick} className={normalPrimary}><MdDownload className='h-7 w-7 py-[2px]'/>&ensp;{name}</button> 
               );
              }
           }else{
             if(buttonType === 'default'){
               return (
-                <button className={normalSecondary}>{name}</button> 
+                <button onClick={onClick} className={normalSecondary}>{name}</button> 
               )
              }else if(buttonType === 'direction'){
               return (
-                <button className={normalSecondary}>{name}&ensp;<FaChevronRight className='h-6 w-6 py-1'/></button> 
+                <button onClick={onClick} className={normalSecondary}>{name}&ensp;<FaChevronRight className='h-6 w-6 py-1'/></button> 
               );
              }else if(buttonType === 'action'){
               return (
-                <button className={normalSecondary}><MdDownload className='h-7 w-7 py-[2px]'/>&ensp;{name}</button> 
+                <button onClick={onClick} className={normalSecondary}><MdDownload className='h-7 w-7 py-[2px]'/>&ensp;{name}</button> 
               );
              }
           }
