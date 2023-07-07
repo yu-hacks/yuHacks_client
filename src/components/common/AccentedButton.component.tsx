231a42d7e4b6b365381c99de1f4deba1d742b2d1
chinyee8 <laichinyee1101@gmail.com>
2023-07-07 08:02:40 -0400

    Create AccentedButton.component.tsx

---
+++ src/components/common/AccentedButton.component.tsx
@@ -0,0 +1,104 @@
+"use client"
+import React, { FC} from 'react';
+import {MdDownload} from 'react-icons/md'
+import {FaChevronRight} from 'react-icons/fa'
+
+// example
+{/* <PrimaryButton name='sec. Direction Button' buttonType='action' primary={true} fullWidth={true}/> */}
+
+type Props = {
+  name: string;
+  buttonType: string;
+  primary: boolean;
+  fullWidth: boolean;
+};
+
+const PrimaryButton: FC<Props> = ({name, buttonType, primary, fullWidth}) => {
+  const normalPrimary = `flex px-12 py-2 transition-all text-white text-base bg-[var(--bg-main-accent)] border-2 border-solid border-black/[.13] shadow-md hover:border-4 focus:border-4 focus:shadow-lg rounded-xl font-HindMadurai font-semibold`;
+  const normalSecondary =`flex px-12 py-2 transition-all text-black text-base hover:shadow-md hover:rounded-xl focus:shadow-lg focus:rounded-xl font-HindMadurai font-semibold`;
+  const fullPrimary = 'w-full w-min-[300px] flex justify-center py-2 transition-all text-white text-base bg-[var(--bg-main-accent)] border-2 border-solid border-black/[.13] shadow-md hover:border-4 focus:border-4 focus:shadow-lg rounded-xl font-HindMadurai font-semibold';
+  const fullSecondary = 'w-full w-min-[300px] flex justify-center py-2 transition-all text-black text-base hover:shadow-md hover:rounded-xl focus:shadow-lg focus:rounded-xl font-HindMadurai font-semibold';
+  const full = 'px-[35px] py-[25px] sm:px-5 sm:w-1/2 md:w-4/12 lg:4/12 items-center justify-center';
+         if(fullWidth){
+          if(primary){
+            if(buttonType === 'default'){
+              return (
+                <div className={full}>
+                  <button className={fullPrimary}>{name}</button> 
+                </div>
+              )
+             }else if(buttonType === 'direction'){
+              return (
+                <div className={full}>
+                  <button className={fullPrimary}>{name}&ensp;<FaChevronRight className='h-6 w-6 py-1'/></button> 
+                </div>
+              );
+             }else if(buttonType === 'action'){
+              return (
+                <div className={full}>
+                  <button className={fullPrimary}><MdDownload className='h-7 w-7 py-[2px]'/>&ensp;{name}</button> 
+                </div>
+              );
+             }
+          }else{
+            if(buttonType === 'default'){
+              return (
+                <div className={full}>
+                  <button className={fullSecondary}>{name}</button> 
+                </div>
+              )
+             }else if(buttonType === 'direction'){
+              return (
+                <div className={full}>
+                  <button className={fullSecondary}>{name}&ensp;<FaChevronRight className='h-6 w-6 py-1'/></button> 
+                </div>
+              );
+             }else if(buttonType === 'action'){
+              return (
+                <div className={full}>
+                  <button className={fullSecondary}><MdDownload className='h-7 w-7 py-[2px]'/>&ensp;{name}</button> 
+                </div>
+              );
+             }
+          }
+          
+         }else{
+          if(primary){
+            if(buttonType === 'default'){
+              return (
+                <button className={normalPrimary}>{name}</button> 
+              )
+             }else if(buttonType === 'direction'){
+              return (
+                <button className={normalPrimary}>{name}&ensp;<FaChevronRight className='h-6 w-6 py-1'/></button> 
+              );
+             }else if(buttonType === 'action'){
+              return (
+                <button className={normalPrimary}><MdDownload className='h-7 w-7 py-[2px]'/>&ensp;{name}</button> 
+              );
+             }
+          }else{
+            if(buttonType === 'default'){
+              return (
+                <button className={normalSecondary}>{name}</button> 
+              )
+             }else if(buttonType === 'direction'){
+              return (
+                <button className={normalSecondary}>{name}&ensp;<FaChevronRight className='h-6 w-6 py-1'/></button> 
+              );
+             }else if(buttonType === 'action'){
+              return (
+                <button className={normalSecondary}><MdDownload className='h-7 w-7 py-[2px]'/>&ensp;{name}</button> 
+              );
+             }
+          }
+         }
+      
+         return (
+          <button className={normalPrimary}>unknown</button> 
+        );
+  
+};
+
+
+export default PrimaryButton;
