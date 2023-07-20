a4386f8417b0f70c9146dd1a4dcd037da98a0ed5
K1fight <92151239+K1fight@users.noreply.github.com>
2023-07-19 22:52:45 -0400

    dashboard-1.0

--- src/components/common/Dashboard.component.tsx
+++ src/components/common/Dashboard.component.tsx
@@ -7,8 +7,11 @@ import {
 } from "@material-tailwind/react";
 import authorsTableData from "@/components/common/authorsTableData";
 import bug from '@/public/images/LinkedIn.png';
+import {FaCheck, FaCheckCircle} from "react-icons/fa";
+import {color} from "framer-motion";
 
 export function Dashboard() {
+
     return (
         <div className="mt-12 mb-8 flex flex-col gap-12">
             <Card>
@@ -69,24 +72,25 @@ export function Dashboard() {
                                             </Typography>
                                         </td>
                                         <td className={className}>
+
                                                 <div>
-                                                    <Avatar src={bug} alt={bug} size={"sm"}/>
+                                                    {website ?<FaCheckCircle style={{color:"green"}}/>:<div></div>}
                                                 </div>
                                         </td>
                                         <td className={className}>
-                                            <Typography className=" text-xs font-semibold text-blue-gray-600">
-                                                {git}
-                                            </Typography>
+                                            <div>
+                                                {git ?<FaCheckCircle style={{color:"green"}}/>:<div></div>}
+                                            </div>
                                         </td>
                                         <td className={className}>
-                                            <Typography className=" text-xs font-semibold text-blue-gray-600">
-                                                {linkedin}
-                                            </Typography>
+                                            <div>
+                                                {linkedin ?<FaCheckCircle style={{color:"green"}}/>:<div></div>}
+                                            </div>
                                         </td>
                                         <td className={className}>
-                                            <Typography className=" text-xs font-semibold text-blue-gray-600">
-                                                {resume}
-                                            </Typography>
+                                            <div>
+                                                {resume ?<FaCheckCircle style={{color:"green"}}/>:<div></div>}
+                                            </div>
                                         </td>
                                         <td className={className}>
                                             <Typography className=" text-xs font-semibold text-blue-gray-600">
