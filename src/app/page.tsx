b290751c1dd9a3ab683d83ffdfaed24c86215802
K1fight <92151239+K1fight@users.noreply.github.com>
2023-07-19 20:14:26 -0400

    first Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import AboutSection from "@/components/sections/AboutSection";
 import HeroSection from "@/components/sections/HeroSection";
 import FaqSection from "@/components/sections/FaqSection";
+import Dashboard from "@/components/common/Dashboard.component";
 
 export default function Home() {
   
   return (
     <main className="flex  min-h-screen flex-col px-auto">
-      {/* <div className="bg-red-500">Hello world</div> */}
-      <HeroSection />
-      <AboutSection />
-      <FaqSection />
+       <div className="bg-red-500">Hello world</div>
+            <Dashboard></Dashboard>
     </main>
   )
 }
