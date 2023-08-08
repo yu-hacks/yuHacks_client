37115d5eaa548db5b86757b513830e9df3298988
Tom Guo <gywghhb@gmail.com>
2023-08-08 05:20:35 -0400

    Replace placeholders in all Sections

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -7,7 +7,7 @@ import img9 from "@/public/images/9.png";
 import img59 from "@/public/images/59.png";
 import imgdec from "@/public/images/56.3.png";
 import major from "@/public/images/major.png";
-import school1 from '@/public/images/school1.png';
+import school1 from "@/public/images/school1.png";
 
 const AboutSection: FC = () => {
   return (
@@ -24,12 +24,15 @@ const AboutSection: FC = () => {
           <h3 className="font-semibold text-4xl text-[#EFCCCC] mb-10 max-w-[450px]">
             Create, Connect, Code in a 36-Hour Adventure
           </h3>
-          <p className="text-lg text-[#C3C3C3] max-w-[500px]">
-            Welcome to yuHacks 2023, York University&apos;s premier 36-hour
-            hackathon! This fall, join 250 innovative and creative developers,
-            designers, and creators to unite and unleash their creativity,
-            collaborating on cutting-edge projects and explore new
-            possibilities!
+          <p className="text-lg text-[#C3C3C3] max-w-[560px] mb-4">
+            yuHacks is the largest hackathon at York University. This fall, we
+            are bringing hundreds of developers, designers, and ideators to
+            explore what’s possible in a 36-hour hackathon.
+          </p>
+          <p className="text-lg text-[#C3C3C3] max-w-[560px]">
+            Whether you’re an experienced hacker or someone who’s learning the
+            rope, we have a place for your here and eager see what inspiring
+            project you’ll bring to the presentation booth.
           </p>
         </div>
       </div>
@@ -37,16 +40,19 @@ const AboutSection: FC = () => {
         id="about-stats-background"
         className="flex flex-col md:flex-row w-full mx-auto bg-white/[0.13] rounded-3xl mt-6 md:mt-0 md:basis-1/3 lg:basis-1/4"
       >
-        <div id="stats-text-container" className="flex flex-col justify-start align-start px-8 py-12">
+        <div
+          id="stats-text-container"
+          className="flex flex-col justify-start align-start px-8 py-12"
+        >
           <h4 className="text-[1.25rem] text-[#717171] font-semibold mb-6">
             Statistics from yuHacks 2022
           </h4>
           <div id="stats-country" className="flex flex-row mb-6">
             <div className="pr-4 shrink-0">
-              <Image src={country} alt="country icon" width={40} height={40}/>
+              <Image src={country} alt="country icon" width={40} height={40} />
             </div>
             <div>
-              <Image src={img9} alt=""/>
+              <Image src={img9} alt="" />
               <p className="text-[#717171] text-md font-HindMadurai mt-3">
                 Countries
               </p>
@@ -54,7 +60,7 @@ const AboutSection: FC = () => {
           </div>
           <div id="stats-schools" className="flex flex-row mb-6">
             <div className="pr-4 shrink-0">
-              <Image src={school1} alt="country icon" width={40} height={40}/>
+              <Image src={school1} alt="country icon" width={40} height={40} />
             </div>
             <div>
               <Image src={img59} alt="" />
@@ -65,7 +71,7 @@ const AboutSection: FC = () => {
           </div>
           <div id="stats-majors" className="flex flex-row">
             <div className="pr-4 shrink-0">
-              <Image src={major} alt="country icon" width={40} height={40}/>
+              <Image src={major} alt="country icon" width={40} height={40} />
             </div>
             <div>
               <Image src={imgdec} alt="" />
--- src/components/sections/faqSection.tsx
+++ src/components/sections/faqSection.tsx
@@ -15,21 +15,42 @@ interface questions {
 const faqData: questions[] = [
   {
     id: 1,
-    question: "What is yuHacks?",
+    question: "What kind of accomodation will be provided?",
     answer:
-      "yuHacks(2023) is the sixth iteration of this student-led event. Our goal is to provide a fun, inclusive, and growth-focused experience that prepares participants for their future tech careers.",
+      "Since yuHacks(2023) is coming back to in-person format, we are working hard to provide a safe and comfortable environment for all our hackers. We will be providing sleeping areas, meals, and other necessities. More detail coming soon.",
   },
-  { id: 2, question: "Who can participate?", answer: "ans1" },
+  {
+    id: 2,
+    question: "Who can participate?",
+    answer:
+      "Any student who's interested in tech or has an innovative idea can participate in yuHacks. We welcome people in all areas of study with different talents to collaborate and come up with the best solution they can produce.",
+  },
   {
     id: 3,
     question: "What if I have never participated in a hackathon before?",
-    answer: "ans2",
+    answer:
+      "yuHacks aims to be beginner-friendly. We will be hosting workshops and providing mentors to help you get started, whether it is in coding, design, or creative thinking.",
   },
 ];
 const faqData2: questions[] = [
-  { id: 4, question: "What kind of activities will there be?", answer: "ans3" },
-  { id: 5, question: "How many people can be on one team?", answer: "ans4" },
-  { id: 6, question: "What if I am not in Ontario?", answer: "ans5" },
+  {
+    id: 4,
+    question: "What kind of activities will there be?",
+    answer:
+      "We will be hosting workshops in various topics in hope to help a wide range of possible project ideas. We will also be having leisure activities to provide a much-needed break for our hackers. Stay tune to learn more.",
+  },
+  {
+    id: 5,
+    question: "How many people can be on one team?",
+    answer:
+      "To stay true to our theme of collaboration and connection, we recommend having at least 2 and up to 4 people in a team. If you can't find a team by the time hackathon starts, we will help you connect with fellow hackers to make sure you have the same experience as everyone.",
+  },
+  {
+    id: 6,
+    question: "Are you accepting applications outside of Ontario, Canada?",
+    answer:
+      "We are open to all students who are in Ontario by the time hackathon starts as it's the easiest to plan logistically. We are still discussing transportation outside Ontario. Stay tune to learn more.",
+  },
 ];
 
 function open(id: string, arrowid: string) {
@@ -52,48 +73,45 @@ const FAQ: FC = () => {
   const [faq2, setFaqs2] = useState<questions[]>(faqData2);
 
   return (
-    <div 
-      id="faq-container"
-      className="flex w-full mx-auto px-3 lg:px-20 py-6"
-    >
-      <div 
+    <div id="faq-container" className="flex w-full mx-auto px-3 lg:px-20 py-6">
+      <div
         id="faq-background"
         className="flex flex-col md:flex-row w-full mx-auto bg-white/[0.13] rounded-3xl"
-      >  
-          <div className=" min-[320px]:p-3 sm:p-8 md:p-5 align-middle">
-            <h1 className="text-[#EFCCCC] text-4xl font-semibold text-center lg:mx-20 mt-5">
-              Frequently asked questions
-            </h1>
-            <br />
-            <br />
+      >
+        <div className="p-8 align-middle">
+          <h1 className="text-[#EFCCCC] text-4xl font-semibold text-center lg:mx-20 mt-5">
+            Frequently Asked Questions
+          </h1>
+          <br />
+          <br />
 
-            <div className="flex-wrap grid-container justify-center lg:mx-10 mb-8">
-              <div>
-                {faq.map((q, index) => {
-                  return (
-                    <Question
-                      question={q.question}
-                      answer={q.answer}
-                      id={q.id}
-                      key={q.id}
-                    />
-                  );
-                })}
-              </div>
-              <div>
-                {faq2.map((q, index) => {
-                  return (
-                    <Question
-                      question={q.question}
-                      answer={q.answer}
-                      id={q.id}
-                      key={q.id}
-                    />
-                  );
-                })}
-              </div>
+          <div className="flex-wrap grid-container justify-center lg:mx-10 mb-8">
+            <div>
+              {faq.map((q, index) => {
+                return (
+                  <Question
+                    question={q.question}
+                    answer={q.answer}
+                    id={q.id}
+                    key={q.id}
+                  />
+                );
+              })}
+            </div>
+            <div>
+              {faq2.map((q, index) => {
+                return (
+                  <Question
+                    question={q.question}
+                    answer={q.answer}
+                    id={q.id}
+                    key={q.id}
+                  />
+                );
+              })}
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
